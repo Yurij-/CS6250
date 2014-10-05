4ea0d7aa8ff47107dc6489df2c00b3820dc18003
nampho2 <nampho@me.com>
2014-10-04 18:31:31 -0700

    added UDP test code

--- assignment-5/iperf.sh
+++ assignment-5/iperf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 #qsize=$1
+# original iperf test using TCP
 iperf -c 10.0.0.2 -p 5001 -t 3600 -i 1 -w 16m -Z reno > iperf.txt &
+
+# extra iperf test using UDP
+#iperf -c 10.0.0.2 -u -b 1.5m -p 5001 -t 3600 -i 1 > iperf.txt &
 echo started iperf
 
